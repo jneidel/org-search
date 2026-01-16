ae5d254900c205a6815546c6997da48dff1912b5
Jonathan Neidel <jneidel@jneidel.com>
2026-01-16 08:42:22 +0100

    Configure colorscheme in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my: {
+          grey: '#1c1c1c',
+          white: '#d0d0d0',
+          blue: '#005fd7',
+          'bright-green': '#00ff5f',
+          'light-red': '#ff5f5f',
+          'bright-red': '#d70000',
+          yellow: '#ffd700',
+          magenta: '#ff005f',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
